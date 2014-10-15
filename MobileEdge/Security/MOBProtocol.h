eed25c868579d59a492b422f4cb897c18fd93660
Raphael Arias <raphael.arias@e-mundo.de>
2014-10-15 10:01:58 +0200

    renamed class extension

---
+++ MobileEdge/Security/MOBProtocol.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 eMundo
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * eMundo. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use it
+ * only in accordance with the terms of the licence agreement you
+ * entered into with eMundo.
+ *
+ * Created by Raphael Arias on 15/10/14.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface MOBProtocol : NSObject
+
+@end
