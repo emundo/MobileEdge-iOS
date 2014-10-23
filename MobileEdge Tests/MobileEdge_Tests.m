1892bdd5857d9153c3d1c7b6f8e7a6cccaf46a99
Raphael Arias <raphael.arias@e-mundo.de>
2014-10-23 16:56:06 +0200

    removed broken test target and added replacement

---
+++ MobileEdge Tests/MobileEdge_Tests.m
@@ -0,0 +1,40 @@
+//
+//  MobileEdge_Tests.m
+//  MobileEdge Tests
+//
+//  Created by Raphael Arias on 23/10/14.
+//  Copyright (c) 2014 eMundo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface MobileEdge_Tests : XCTestCase
+
+@end
+
+@implementation MobileEdge_Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
