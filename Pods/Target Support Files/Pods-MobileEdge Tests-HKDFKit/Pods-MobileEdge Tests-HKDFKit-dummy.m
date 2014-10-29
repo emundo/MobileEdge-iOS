a90c605a7c72d7ccc172b92305b0936a4961f58e
Raphael Arias <raphael.arias@e-mundo.de>
2014-10-29 10:48:19 +0100

    Pods for Test taget

---
+++ Pods/Target Support Files/Pods-MobileEdge Tests-HKDFKit/Pods-MobileEdge Tests-HKDFKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileEdge_Tests_HKDFKit : NSObject
+@end
+@implementation PodsDummy_Pods_MobileEdge_Tests_HKDFKit
+@end
---
+++ Pods/Target Support Files/Pods-MobileEdge Tests-SQLCipher/Pods-MobileEdge Tests-SQLCipher-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MobileEdge_Tests_SQLCipher : NSObject
+@end
+@implementation PodsDummy_Pods_MobileEdge_Tests_SQLCipher
+@end
