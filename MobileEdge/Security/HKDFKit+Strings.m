52f11c822df75fbc9aea0504ffa10867ced3a8fe
Raphael Arias <raphael.arias@e-mundo.de>
2014-11-04 15:55:04 +0100

    category to simplify key derivation code

---
+++ MobileEdge/Security/HKDFKit+Strings.m
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2014 eMundo
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * eMundo. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use it
+ * only in accordance with the terms of the licence agreement you
+ * entered into with eMundo.
+ *
+ * Created by Raphael Arias on 04/11/14.
+ */
+
+#import "HKDFKit+Strings.h"
+
+@implementation HKDFKit (Strings)
+
+@end
