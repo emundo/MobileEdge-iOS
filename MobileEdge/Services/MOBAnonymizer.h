00ffbafaf566cc40e229e7265b7978e686252dd2
Raphael Arias <raphael.arias@e-mundo.de>
2014-11-11 18:03:26 +0100

    copyright header fix in new file

--- MobileEdge/Services/MOBAnonymizer.h
+++ MobileEdge/Services/MOBAnonymizer.h
@@ -1,10 +1,23 @@
-//
-//  MOBAnonymizer.h
-//  MobileEdge
-//
-//  Created by Raphael Arias on 11/11/14.
-//  Copyright (c) 2014 eMundo. All rights reserved.
-//
+/*
+ * Copyright (c) 2014 eMundo
+ * All Rights Reserved.
+ *
+ * This file is part of MobileEdge-iOS.
+ * MobileEdge-iOS is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MobileEdge-iOS is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with MobileEdge-iOS.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Created by Raphael Arias on 11/11/14.
+ */
 
 #import <Foundation/Foundation.h>
 #import "MOBAnonymizerSettings.h"
