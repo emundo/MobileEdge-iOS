3f0ec0d8ec54f1d2ed7641a40a581c088ab8a501
Raphael Arias <raphael.arias@e-mundo.de>
2015-02-27 08:43:03 +0100

    add category on NACLKeyPairs.
    we probably don't really need this...

---
+++ MobileEdge-iOS/Security/NACLKeyPair+Base64Dictionary.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2015 eMundo
+ * All Rights Reserved.
+ *
+ * This software is the confidential and proprietary information of
+ * eMundo. ("Confidential Information"). You
+ * shall not disclose such Confidential Information and shall use it
+ * only in accordance with the terms of the licence agreement you
+ * entered into with eMundo.
+ *
+ * Created by Raphael Arias on 24/02/15.
+ */
+
+#import "NACLKeyPair.h"
+
+@interface NACLKeyPair (Base64Dictionary)
+
+@end
